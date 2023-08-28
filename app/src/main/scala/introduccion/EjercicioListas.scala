789c40213684b5dde4f48ece7eb93f78dd069c5b
Pbernal30980 <pbernal309@gmail.com>
2023-08-28 12:31:19 -0500

    primera_asignacion_finished

--- app/src/main/scala/introduccion/EjercicioListas.scala
+++ app/src/main/scala/introduccion/EjercicioListas.scala
@@ -1,6 +1,7 @@
 package introduccion
 
 import javax.naming.OperationNotSupportedException
+import scala.annotation.meta.param
 
 class EjercicioListas {
   /*
@@ -12,9 +13,14 @@ class EjercicioListas {
   * @throws IllegalArgumentException si n es negativo
   */
   def repetirListas(lista: List[Int], n: Int): List[List[Int]] = {
-    var listaRepetida : List[List[Int]] = List()
-    //Complete el código
-    throw new UnsupportedOperationException("No implementado aún")
+    if (n < 0) {
+      throw new IllegalArgumentException("El valor de n debe ser mayor o igual a 0")
+    }
+    var listaRepetida: List[List[Int]] = List()
+    for (i <- lista) {
+      listaRepetida = listaRepetida :+ List.fill(n)(i)
+    }
+    return listaRepetida
   }
   /*
   * Punto 3: Filtrar listas
@@ -25,10 +31,19 @@ class EjercicioListas {
   * @throws IllegalArgumentException si el criterio no es uno de los valores válidos
   */
 
-  def filtrarListas(criterioIn: String, n: Int, lista: List[Int]) : List[Int] = {
-    var criterio : String = criterioIn.toLowerCase()
-    var listaFiltrada : List[Int] = List()
-    //Complete el código
-    throw new UnsupportedOperationException("No implementado aún")
+  def filtrarListas(criterioIn: String, n: Int, lista: List[Int]): List[Int] = {
+    var criterio: String = criterioIn.toLowerCase()
+    var listaFiltrada: List[Int] = List()
+    criterio match {
+      case "mayor" => listaFiltrada = lista.filter(_ > n)
+      case "menor" => listaFiltrada = lista.filter(_ < n)
+      case "mayoroigual" => listaFiltrada = lista.filter(_ >= n)
+      case "igual" => listaFiltrada = lista.filter(_ == n)
+      case "diferente" => listaFiltrada = lista.filter(_ != n)
+      case "menoroigual" => listaFiltrada = lista.filter(_ <= n)
+      case _ => throw new IllegalArgumentException("Criterio no válido")
+    }
+    return listaFiltrada
   }
+
 }
